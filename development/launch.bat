f6833f6b2bbc1bf0848f98e95ec11567f2b66e1c
Michael Palumbo <michael.palumbo87@gmail.com>
2012-07-19 22:34:03 +0300

    Update development/launch.bat

--- development/launch.bat
+++ development/launch.bat
@@ -5,7 +5,6 @@ set TOOLS_ROOT=%~dp0\tools
 set PROJECTS_ROOT=%~dp0\projects
 set GIT_ROOT=%TOOLS_ROOT%\PortableGit
 set PYTHON_ROOT=%TOOLS_ROOT%\PortablePython
-set POSTGRESQL_ROOT=%TOOLS_ROOT%\postgresq
 set VIRTUALENVS_ROOT=%TOOLS_ROOT%\virtualenvs
 
 REM : SET PATH FOR ALL THE VIRTUAL ENVS
@@ -17,14 +16,12 @@ REM
 REM : DO NOT MODIFY BELOW UNLESS YOU KNOW WHAT YOU ARE DOING
 REM
 PATH=%PYTHON_ROOT%\App;%PYTHON_ROOT%\App\Scripts;%PATH%
-PATH=%POSTGRESQL_ROOT%\bin;%PATH%
 
 doskey shell=python manage.py shell
 doskey dbshell=python manage.py dbshell
 doskey shell_plus=python manage.py shell_plus
 doskey syncdb=python manage.py syncdb
 doskey runserver=python manage.py runserver
-set runserver=python manage.py runserver
 doskey cleanpyc=del *.pyc /q/a/s
 
 IF "%1"=="" GOTO NOPROJECT
